a29800f9eed8f9c5e42cd4ae5b6da2c5c4dcfb57
sheizan <adfx-02morgan>
2019-09-14 17:19:37 +0700

    zuul and eureka setup done

--- src/main/java/com/rifkiansyah/main/MainApplication.java
+++ src/main/java/com/rifkiansyah/main/MainApplication.java
@@ -3,7 +3,9 @@ package com.rifkiansyah.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 @EnableEurekaServer
 public class MainApplication {
